4e957d49810a33e3eba316cf81353eb305ad108a
KZ-DE <nebulacom005@gmail.com>
2024-01-28 11:44:17 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+// main file
+import "./App.css";
+import Introduc from "./components/Introduc";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Introduc />
     </div>
   );
 }
